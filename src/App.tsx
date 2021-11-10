4dea5eb0c28a30ef91ffc8bd28d95fbe99802229
Washington Mendes <washington.mendes@zigpay.com.br>
2021-11-09 22:09:19 -0300

    Adicionado as estilizações globais

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { GlobalStyle } from "./styles/global";
 
 export const App = () => {
   return (
-    <div>
+    <div className="App">
       <GlobalStyle/>
       <h1>Hello Word!</h1>
     </div>
--- src/styles/global.ts
+++ src/styles/global.ts
@@ -31,9 +31,18 @@ export const GlobalStyle = createGlobalStyle`
     }
 
     body{
-        background: var(---background);
+        background: var(--background);
         -webkit-font-smoothing:antialiased;
         
+    }
+
+    body, input, textarea, button{
+        font-family:'Poppins', sans-serif;
+        font-weight:400;
+    }
+
+    h1, h2, h3, h4, h5, h6, strong{
+        font-weight:600;
     }
 
     button{
@@ -45,4 +54,4 @@ export const GlobalStyle = createGlobalStyle`
         opacity:0.6;
         cursor: not-allowed;
     }
-`;
+`
