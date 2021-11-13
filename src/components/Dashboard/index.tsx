85df3e9099d93312c713f9f64c80c7a61318b3c9
Washington Mendes <washington.mendes@zigpay.com.br>
2021-11-13 02:00:15 -0300

    Adicionado o componente de TransactionsTable

--- src/components/Dashboard/index.tsx
+++ src/components/Dashboard/index.tsx
@@ -1,9 +1,11 @@
 import { Summary } from "../Summary";
+import { TransactionsTable } from "../TransactionsTable";
 import { Container } from "./styled";
 export const Dashboard = () => {
   return (
     <Container>
       <Summary />
+      <TransactionsTable/>
     </Container>
   );
 };
---
+++ src/components/TransactionsTable/index.tsx
@@ -0,0 +1,31 @@
+import { Container } from "./styled";
+export const TransactionsTable = () => {
+  return (
+    <Container>
+      <table>
+        <thead>
+          <tr>
+            <th>Título</th>
+            <th>Valor</th>
+            <th>Categoria</th>
+            <th>Data</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>Desenvolvimento de Website</td>
+            <td className="deposit">R$ 12.000,00</td>
+            <td>Desenvolvimento</td>
+            <td>10/04/2021</td>
+          </tr>
+          <tr>
+            <td>Hamburguer</td>
+            <td className="withdraw">-R$ 59,00</td>
+            <td>Alimentação</td>
+            <td>20/04/2021</td>
+          </tr>
+        </tbody>
+      </table>
+    </Container>
+  );
+};
