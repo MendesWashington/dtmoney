10782bbf8bed66a155bd7780ecb9768f87c69150
mendesswashington <mendesswashington@gmai.com>
2021-11-14 17:58:00 -0300

    Adicionado ao modal os botões de saída e entrada

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -1,5 +1,8 @@
 import Modal from "react-modal";
-import { Container } from "./styled";
+import closeImg from "../../assets/close.svg";
+import inComeImg from "../../assets/income.svg";
+import outComeImg from "../../assets/outcome.svg";
+import { Container, TransactionTypeContainer } from "./styled";
 
 interface NewTransactionModalProps {
   isOpen: boolean;
@@ -16,10 +19,34 @@ export const NewTransactionModal = ({
     <Modal
       isOpen={isOpen}
       onRequestClose={onRequestClose}
+      overlayClassName="react-modal-overlay"
+      className="react-modal-content"
     >
-     <Container>
-       <h2>Transação</h2>
-     </Container>
+      <button
+        type="button"
+        onClick={onRequestClose}
+        className="react-modal-close"
+      >
+        <img src={closeImg} alt="Fechar modal" />
+      </button>
+      <Container>
+        <h2>Cadastro de Transação</h2>
+
+        <input type="text" name="name" placeholder="Título" />
+        <input type="number" name="value" placeholder="Valor" />
+        <TransactionTypeContainer>
+          <button type="button">
+            <img src={inComeImg} alt="Entrada" />
+            <span>Entrada</span>
+          </button>
+          <button type="button">
+            <img src={outComeImg} alt="Saída" />
+            <span>Saída</span>
+          </button>
+        </TransactionTypeContainer>
+        <input type="text" name="category" placeholder="Categoria" />
+        <button type="submit">Cadastrar</button>
+      </Container>
     </Modal>
   );
 };
